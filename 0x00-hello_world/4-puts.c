73a129a13965f5e4b4d2e4303cbcc3e9638d2112
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-09 16:48:53 -0700

    My task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -5,10 +5,10 @@
  *
  * Description: using the main function
  * this program prints "Programming is like building a multilingual puzzle"
- * Reurn: Always 0 (success:)
+ * Return: Always 0 (success:)
  */
 int main(void)
 {
-	puts("Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle\n");
 	return (0);
 }
