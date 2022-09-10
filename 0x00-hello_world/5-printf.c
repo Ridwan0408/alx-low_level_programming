15f91f801291f5df0bcf65ca172cd5b9e8921092
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-09 18:50:29 -0700

    My task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
+
 /**
- * main -print the string with the printf function
+ * main - print the string with the printf function
  *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,"
@@ -9,5 +10,5 @@
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return (0);
+	return(0);
 }
