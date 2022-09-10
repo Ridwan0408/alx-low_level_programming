7e4a7be5dc5fea40d2e8c89a710f54cd6c3c50e5
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-09 17:33:57 -0700

    My task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 
 /**
- * main -print the string in the printf function
+ * main -print the string with the printf function
  *
  * Description: using the main function
  * this program prints "with proper grammar, but the outcome is a piece of art,"
- * Return: Always 0 (success)
+ * Return: Always 0 (success:)
  */
 int main(void)
 {
