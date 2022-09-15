ab09168d0542892727e55da395a376508796871b
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-15 05:19:57 -0700

    My Task 5

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,11 +2,13 @@
 
 /**
  * print_sign - function that prints the sign of a number
+ * @n: is the int that will use for the argument of the function
  *
  * Return: 0
  */
 int print_sign(int n)
 {
+
 	if (n > 0)
 	{
 		_putchar ('+');
@@ -14,7 +16,7 @@ int print_sign(int n)
 	}
 	else if (n == 0)
 	{
-		_putchar (0);
+		_putchar ('0');
 		return (0);
 	}
 	else
