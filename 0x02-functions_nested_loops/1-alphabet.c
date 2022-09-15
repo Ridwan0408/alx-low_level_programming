da802ae3365c69e51d9a6b84bad572e78a69bfbb
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-15 03:29:58 -0700

    My Task 1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet
+ *
+ * Description: using main function
+ * this program prints alphabets in lower case.
+ *
+ * Return: Always 0
+ */
+
+void print_alphabet(void)
+{
+	char n;
+	
+	for (n = 'a'; n <= 'z'; n++)
+	{
+		_putchar (c);
+	}
+	_putchar ('\n');
+
+	return (0);
+}
