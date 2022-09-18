d28a16513e501922248c00efa7310560c350a522
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-18 11:54:57 -0700

    My Task 0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isupper - a function that checks for uppercase character
+ * @c: the character to be tested
+ * Return: 1 if it uppercase, 0 otherwise
+ */
+
+int _isupper(int c);
+
+{
+
+	if ((c >= 'A') && (c <= 'Z'))
+	{
+		return (1);
+	}
+
+	return (0);
+}
---
+++ 0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,22 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+ * main - header files for prototype
+ * void prototypes (void)
+ * int prototypes (int)
+ */
+
+int _isupper(int);
+int _isdigit(int);
+int _putchar(char);
+int mul(int);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif /* MAIN_H */
