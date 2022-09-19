cfd1502661060bccec6dbd2c7cc1e06272e45dfc
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-18 17:19:38 -0700

    My Task 5

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -9,15 +9,15 @@
 
 void more_numbers(void)
 {
-	int n1, n2
+	int n1, n2;
 
 	for (n1 = 48; n1 <= 57; n1++)
 	{
-		for (n1 = 48; n2 <= 62; a++)
+		for (n2 = 48; n2 <= 62; n2++)
 		{
-			putchar (n1);
+			putchar (n2);
 		}
-		putchar (n2);
+		putchar (n1);
 	}
 	putchar ('\n');
 }
