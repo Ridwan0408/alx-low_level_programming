7cfc3a56d3e90e8a9f37c9e7028a4d9d66b23123
Ridwan0408 <gbolahanridwan248@gmail.com>
2022-09-20 03:35:38 -0700

    My Task 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,16 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _puts -  a function that prints a string, followed by a new line, to stdout
+ * @str: a parameter to _puts function
+ */
+
+void _puts(char *str)
+{
+	for (*str != '\0'; str++;)
+	{
+		_putchar (*str);
+	}
+	_putchar ('\n');
+}
